8df493854e518deceef669d5df241cb3b881889b
Munti1 <alexandruioan.muntean@ulbsibiu.ro>
2024-11-04 23:31:15 +0200

    .

--- Laboratory project 2/Program.cs
+++ Laboratory project 2/Program.cs
@@ -11,9 +11,10 @@ namespace Library
     static void Main(string[]args)
     {
 
-         Book book1 = new Book ("The Great Gatsby","F. Scott Fitzgerald");
-
-         Book book2 = new Book ("Ulysses","James Joyce",720);
+        Book book1 = new Book ("The Great Gatsby","F. Scott Fitzgerald");
+        book1.CheckOut();
+        Book book2 = new Book ("Ulysses","James Joyce",720);
+        book2.ReturnBook();
 
         Console.WriteLine(book1.ToString());
         Console.WriteLine(book2.ToString());
